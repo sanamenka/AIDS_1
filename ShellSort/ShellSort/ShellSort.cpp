de36d776733800fb743a8be77d10c6172bea43d4
sanamenka <vega_gera@mail.ru>
2025-04-29 18:39:27 +0700

    Реализация сортировки Шелла

--- ShellSort/ShellSort/ShellSort.cpp
+++ ShellSort/ShellSort/ShellSort.cpp
@@ -1,5 +1,42 @@
+#include <iostream>
 
-main() {
+void shellSort(int* array, int size) {
+    for (int gap = size / 2; gap > 0; gap /= 2) {
+        for (int j = gap; j < size; j++) {
+            for (int k = j - gap; k >= 0; k -= gap) {
+                if (array[k] < array[k + gap]) {
+                    break;
+                }
+                else {
+                    int temp = array[k + gap];
+                    array[k + gap] = array[k];
+                    array[k] = temp;
+                }
+            }
+        }
+    }
+}
 
-	return 0;
+int main() {
+    int size;
+    std::cout << "How many numbers you want to enter into the unsorted array: " << std::endl;
+    std::cin >> size;
+
+    int* array = new int[size];
+
+    std::cout << "Enter the numbers for the unsorted array: " << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cin >> array[i];
+    }
+    std::cout << "Unsorted array: " << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << " ";
+    }
+    shellSort(array, size);
+    std::cout << "\nSorted array: " << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << " ";
+    }
+    delete[] array;
+    return 0;
 }
